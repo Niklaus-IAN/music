323d9a6518ff06304f15c1a3ea190b61438ac78b
Niklaus-IAN <105167056+Niklaus-IAN@users.noreply.github.com>
2022-06-17 08:30:22 +0100

    Add files via upload

---
+++ order.js
@@ -0,0 +1,24 @@
+$("a").hover(function(){
+    $(this).css("text-decoration","none");},
+    function(){
+        $(this).css("text-decoration", "underline")
+})
+
+// $("button").hover(function(){
+//     $("button").addClass("tesa");},
+//     function(){
+//         $("button").addClass("tesy");
+// })
+
+$("button").hover(function(){
+    $(this).css("background-color","rgb(91, 91, 128)");},
+    function(){
+        $(this).css("background-color", "rgb(56, 41, 224)")
+})
+
+$(".chii p").hover(function(){
+    $(this).css("color","black");},
+    function(){
+        $(this).css("color", "rgb(133, 143, 146)")
+})
+
